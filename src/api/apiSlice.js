314a96354c4f380f88c475608a53b786f15c6665
Vikas Sinha <sinhavikas234@gmail.com>
2026-02-10 19:13:10 +0530

    frontend_dash-update

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/api',
+    baseUrl: 'https://school-backend-dashboard.onrender.com/api',
     prepareHeaders: (headers) => {
       const token = sessionStorage.getItem('token');
       if (token) {
